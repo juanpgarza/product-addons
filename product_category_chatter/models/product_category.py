fda9c2f1c6e79625c2342af27b9fa43669c4b129
Juan Pablo Garza <juanp@juanpgarza.com>
2024-06-28 11:22:47 -0300

    [15.0][IMP] product_category_chatter: que loguee el cambio de nombre

--- product_category_chatter/models/product_category.py
+++ product_category_chatter/models/product_category.py
@@ -2,8 +2,10 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import models
+from odoo import models, fields
 
 class ProductCategory(models.Model):
     _name = 'product.category'
     _inherit = ['product.category', 'mail.thread', 'mail.activity.mixin']
+
+    name = fields.Char(tracking=True)
