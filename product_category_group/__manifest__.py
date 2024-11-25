955d5742473e9be692d421708090db7333d98c7d
Juan Pablo Garza <juanp@juanpgarza.com>
2024-11-25 14:12:33 +0000

    [MIG] product_category_group: Migration to 17.0

--- product_category_group/__manifest__.py
+++ product_category_group/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Product Category Group",
     "summary": "",
-    "version": "15.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Product",
     "website": "https://github.com/juanpgarza/product-addons",
     "author": "juanpgarza",
