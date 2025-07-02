8ea27ca58e6953846de60c85172dd641a83e3c32
Juan Pablo Garza <jgarza@juanpgarza.com>
2025-07-02 13:45:39 +0000

    [MIG] product_category_group: Migration to 18.0

--- product_category_group/__manifest__.py
+++ product_category_group/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Product Category Group",
     "summary": "",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Product",
     "website": "https://github.com/juanpgarza/product-addons",
     "author": "juanpgarza",
@@ -18,5 +18,5 @@
         'views/product_category_views.xml',        
         'security/ir.model.access.csv',        
         ],
-    "installable": False,
+    "installable": True,
 }
