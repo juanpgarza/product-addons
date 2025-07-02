2c86392272f2d408d8ce1d287b9d5b0211423640
Juan Pablo Garza <jgarza@juanpgarza.com>
2025-07-02 13:45:13 +0000

    [MIG] product_category_chatter: Migration to 18.0

--- product_category_chatter/__manifest__.py
+++ product_category_chatter/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "product_category_chatter",
     "summary": "",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Product",
     "website": "https://github.com/juanpgarza/product-addons",
     "author": "juanpgarza",
@@ -15,5 +15,5 @@
     "data": [
         'views/product_category_views.xml',
         ],
-    "installable": False,
+    "installable": True,
 }
